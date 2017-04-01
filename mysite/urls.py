921d4cc6678ed092fd77adaf102f4a6bb7ef50a7
Yash Agrawal <yagrawal900@gmail.com>
2017-04-01 20:10:49 +0530

    add app

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^polls/', include('polls.urls')),   
+    url(r'^polls/', include('polls.urls')),
+    url(r'^noticeboard/', include('NoticeBoard.urls')),   
 ]
